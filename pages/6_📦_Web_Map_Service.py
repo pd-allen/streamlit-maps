56817c91da6ad1929112a88c724712268199b8fb
pd-allen <43551964+pd-allen@users.noreply.github.com>
2025-02-18 16:18:48 -0500

    Update 6_📦_Web_Map_Service.py

--- pages/6_📦_Web_Map_Service.py
+++ pages/6_📦_Web_Map_Service.py
@@ -5,7 +5,7 @@ import ast
 import streamlit as st
 import time
 import geopandas as gpd
-import leafmap.foliumap as leafmap
+import leafmap.leafmap as leafmap
 import pandas as pd
 import numpy as np
 #import openrouteservice
