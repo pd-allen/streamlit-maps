3b6c4321b57b83a67528391589ca16091f44e2e6
pd-allen <43551964+pd-allen@users.noreply.github.com>
2025-02-18 16:04:38 -0500

    Update 6_📦_Web_Map_Service.py

--- pages/6_📦_Web_Map_Service.py
+++ pages/6_📦_Web_Map_Service.py
@@ -59,8 +59,8 @@ layers = None
 #client = openrouteservice.Client(key='5b3ce3597851110001cf62488f9235173eab4627b5f0ba01e928ef81')
 #fname='1stBttnCEF.xlsx'
 #fname="8thHussarsItaly.xlsx"
-fname ="https://raw.githubusercontent.com/opengeos/data/main/world/world_cities.csv"
-#fname= 'https://pd-allen.github.io/docs/8thHussarsItaly.csv'
+#fname ="https://raw.githubusercontent.com/opengeos/data/main/world/world_cities.csv"
+fname= 'https://raw.githubusercontent.com/pd-allen.github.io/main/docs/8thHussarsItaly.csv'
 #fn=fname[:len(fname)-5]
 #print(fname,fn)
 #fname= os.path.join(excelpath, fname)
