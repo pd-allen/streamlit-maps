dd8585c7607d2de2f1327b5ccee6b1b3a7240915
pd-allen <43551964+pd-allen@users.noreply.github.com>
2025-02-18 15:45:00 -0500

    Update 6_📦_Web_Map_Service.py

--- pages/6_📦_Web_Map_Service.py
+++ pages/6_📦_Web_Map_Service.py
@@ -72,7 +72,8 @@ with row1_col1:
 
     m = leafmap.Map(center=(50.0,3.0), layers_control=True,color_column=False,zoom=8)
 
-    m.add_points_from_xy(data, x="longitude", y="latitude",color="red",popup=["Location","Date", "Comments"],layer_name='points')
+    m.add_points_from_xy(data, x="longitude", y="latitude",color="red",popup=["name","country"],layer_name='points')
+    #m.add_points_from_xy(data, x="Longitude", y="Latitude",color="red",popup=["Location","Date", "Comments"],layer_name='points')
     m
    
 
